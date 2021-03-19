8f19cdfb976b313b5b9fe97f104715ec98294ed9
Wetherk <Wetherk@yandex.by>
2021-03-19 18:07:22 +0300

    fixing bugs

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// input field variables
-const nameInput = document.getElementById('name_input');
-const surNameInput = document.getElementById('surname_input');
-const dateInput = document.getElementById('date_input');
-const weightInput = document.getElementById('weight_input');
 const addBtn = document.getElementById('add_btn');
-// other stuff
 const sortByNameBtn = document.getElementById('name_sort');
 const sortBySurNameBtn = document.getElementById('surname_sort');
 const sortByDateBtn = document.getElementById('date_sort');
@@ -12,17 +6,25 @@ const sortByWeightBtn = document.getElementById('weight_sort');
 const table = document.getElementById('info_table');
 let personArr = [];
 let defaultArr = [];
-let test = '';
-let j = 0;
+let sortPropCheck = 0;
+let sortStatus = 0;
+
 // functions
 function addInfoToObj() {
+ const nameInput = document.getElementById('name_input');
+ const surNameInput = document.getElementById('surname_input');
+ const dateInput = document.getElementById('date_input');
+ const weightInput = document.getElementById('weight_input');
  let personObj = {};
+
  personObj.name = nameInput.value;
  personObj.surname = surNameInput.value;
  personObj.dateofbirth = dateInput.value;
  personObj.weight = weightInput.value;
+
  return personObj;
 }
+
 function tableCreate(arr){
  for (let i = 0; i < arr.length; i++) {
   let row = table.insertRow(i + 1);
@@ -42,38 +44,48 @@ function tableCreate(arr){
   row.appendChild(btn);
  }
 }
+
 function deletingRow(r) {
  let i = r.parentNode.rowIndex;
  table.deleteRow(i);
  personArr = personArr.reverse();
  personArr.splice([i - 1], 1);
  personArr = personArr.reverse();
+ defaultArr = [...personArr];
 }
+
 function deleteTable(length){
  for (let i = length; i > 0;i--){
   table.deleteRow(i);
  }
 }
+
 function check(arr) {
  if (arr.length > 6) {
   console.log('too many users. removing first added');
   personArr.shift();
  }
 }
+
 function sortArr(arr, prop) {
- if (prop != test) j = 0;
- if (arr.length > 1 && j != 'sorted1' && j !='sorted2') {
+ if (prop != sortPropCheck) sortStatus = 0;
+
+ if (arr.length > 1
+   && sortStatus != 'sorted1'
+   && sortStatus !='sorted2'){
   arr.sort((a, b) => (a[prop] > b[prop]) ? 1 : -1);
-  j = 'sorted1';
-  test = prop;
+  sortStatus = 'sorted1';
+  sortPropCheck = prop;
   return arr;
  }
- if (j === 'sorted1'){
-  j = 'sorted2'
+
+ if (sortStatus === 'sorted1'){
+  sortStatus = 'sorted2'
   return arr.reverse();
  }
- if (j === 'sorted2') {
-  j = 0;
+
+ if (sortStatus === 'sorted2') {
+  sortStatus = 0;
   return defaultArr;
  }
 }
@@ -86,19 +98,31 @@ addBtn.addEventListener('click',()=>{
  defaultArr = [...personArr];
  tableCreate(personArr);
 })
+
 sortByNameBtn.addEventListener('click',()=>{
- if (personArr.length > 1) deleteTable(personArr.length);
- tableCreate(sortArr(personArr, 'name'));
+ if (personArr.length > 1) {
+  deleteTable(personArr.length);
+  tableCreate(sortArr(personArr, 'name'));
+ }
 })
+
 sortBySurNameBtn.addEventListener('click',()=>{
- if (personArr.length > 1) deleteTable(personArr.length);
- tableCreate(sortArr(personArr, 'surname'));
+ if (personArr.length > 1){
+  deleteTable(personArr.length);
+  tableCreate(sortArr(personArr, 'surname'));
+ } 
 })
+
 sortByDateBtn.addEventListener('click',()=>{
- if (personArr.length > 1) deleteTable(personArr.length);
- tableCreate(sortArr(personArr, 'dateofbirth'));
+ if (personArr.length > 1){
+  deleteTable(personArr.length);
+  tableCreate(sortArr(personArr, 'dateofbirth'));
+ } 
 })
+
 sortByWeightBtn.addEventListener('click',()=>{
- if (personArr.length > 1) deleteTable(personArr.length);
- tableCreate(sortArr(personArr, 'weight'));
+ if (personArr.length > 1){
+  deleteTable(personArr.length);
+  tableCreate(sortArr(personArr, 'weight'));
+ } 
 })
